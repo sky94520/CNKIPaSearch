e28a5d40e6bd53bc3d54af97e174457b87e73a9f
sky94520 <541052067@qq.com>
2020-04-21 17:05:44 +0800

    docker顺序执行page和detail

--- run_page_and_detail.py
+++ run_page_and_detail.py
@@ -1,18 +1,26 @@
 # --coding:UTF-8--
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
-from scrapy.crawler import CrawlerProcess
+from twisted.internet import defer, reactor
+from CNKIPaSearch.spiders.page import PageSpider
+from CNKIPaSearch.spiders.detail import DetailSpider
 
 
-def start_spider(spider_name):
-    project_settings = get_project_settings()
-    settings = dict(project_settings.copy())
-    # 合并配置
-    process = CrawlerProcess(settings)
-    process.crawl(spider_name)
-    process.start()
+configure_logging()
+project_settings = get_project_settings()
+settings = dict(project_settings.copy())
+runner = CrawlerRunner(settings)
+
+
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(PageSpider)
+    yield runner.crawl(DetailSpider)
+    reactor.stop()
 
 
 if __name__ == '__main__':
-    # 爬取
-    start_spider(spider_name='page')
-    start_spider(spider_name='detail')
+    # 爬取 先获取页面，然后获取具体信息
+    crawl()
+    reactor.run()
